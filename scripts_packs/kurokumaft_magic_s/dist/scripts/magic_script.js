ba992bcf7cec90ccc284fd0b73409b4c084127e3
koukuma5968 <hirano.kosuke.bs@gmail.com>
2025-02-17 14:26:47 +0900

    20250217 releace

--- scripts_packs/kurokumaft_magic_s/dist/scripts/magic_script.js
+++ scripts_packs/kurokumaft_magic_s/dist/scripts/magic_script.js
@@ -11042,29 +11042,63 @@ async function freezeRangeBlock(event) {
 // scripts/items/pickaxe/BreezeMagicPickaxe.ts
 import { BlockVolume as BlockVolume7, EquipmentSlot as EquipmentSlot26 } from "@minecraft/server";
 var polishedStone = [
-  "basalt",
-  "blackstone_bricks",
-  "blackstone_brick_stairs",
-  "blackstone_brick_wall",
-  "blackstone_brick_slab",
-  "blackstone",
-  "blackstone_stairs",
-  "blackstone_slab",
-  "blackstone_pressure_plate",
-  "blackstone_button",
-  "blackstone_wall",
-  "tuff",
-  "tuff_slab",
-  "tuff_stairs",
-  "tuff_wall",
-  "granite_stairs",
-  "diorite_stairs",
-  "andesite_stairs",
-  "deepslate",
-  "deepslate_slab",
-  "deepslate_stairs",
-  "deepslate_wall",
-  "deepslate_double_slab"
+  { polished: "minecraft:polished_andesite", coarse: "minecraft:andesite" },
+  { polished: "minecraft:polished_andesite_double_slab", coarse: "minecraft:andesite_double_slab" },
+  { polished: "minecraft:polished_andesite_slab", coarse: "minecraft:andesite_slab" },
+  { polished: "minecraft:polished_andesite_stairs", coarse: "minecraft:andesite_stairs" },
+  { polished: "minecraft:polished_basalt", coarse: "minecraft:basalt" },
+  { polished: "minecraft:polished_blackstone", coarse: "minecraft:blackstone" },
+  { polished: "minecraft:polished_blackstone_brick_double_slab", coarse: "minecraft:blackstone_brick_double_slab" },
+  { polished: "minecraft:polished_blackstone_brick_slab", coarse: "minecraft:blackstone_brick_slab" },
+  { polished: "minecraft:polished_blackstone_brick_stairs", coarse: "minecraft:blackstone_brick_stairs" },
+  { polished: "minecraft:polished_blackstone_brick_wall", coarse: "minecraft:blackstone_brick_wall" },
+  { polished: "minecraft:polished_blackstone_bricks", coarse: "minecraft:blackstone_bricks" },
+  { polished: "minecraft:polished_blackstone_button", coarse: "minecraft:blackstone_button" },
+  { polished: "minecraft:polished_blackstone_double_slab", coarse: "minecraft:blackstone_double_slab" },
+  { polished: "minecraft:polished_blackstone_pressure_plate", coarse: "minecraft:blackstone_pressure_plate" },
+  { polished: "minecraft:polished_blackstone_slab", coarse: "minecraft:blackstone_slab" },
+  { polished: "minecraft:polished_blackstone_stairs", coarse: "minecraft:blackstone_stairs" },
+  { polished: "minecraft:polished_blackstone_wall", coarse: "minecraft:blackstone_wall" },
+  { polished: "minecraft:polished_deepslate", coarse: "minecraft:deepslate" },
+  { polished: "minecraft:polished_deepslate_double_slab", coarse: "minecraft:deepslate_double_slab" },
+  { polished: "minecraft:polished_deepslate_slab", coarse: "minecraft:deepslate_slab" },
+  { polished: "minecraft:polished_deepslate_stairs", coarse: "minecraft:deepslate_stairs" },
+  { polished: "minecraft:polished_deepslate_wall", coarse: "minecraft:deepslate_wall" },
+  { polished: "minecraft:polished_diorite", coarse: "minecraft:diorite" },
+  { polished: "minecraft:polished_diorite_double_slab", coarse: "minecraft:diorite_double_slab" },
+  { polished: "minecraft:polished_diorite_slab", coarse: "minecraft:diorite_slab" },
+  { polished: "minecraft:polished_diorite_stairs", coarse: "minecraft:diorite_stairs" },
+  { polished: "minecraft:polished_granite", coarse: "minecraft:granite" },
+  { polished: "minecraft:polished_granite_double_slab", coarse: "minecraft:granite_double_slab" },
+  { polished: "minecraft:polished_granite_slab", coarse: "minecraft:granite_slab" },
+  { polished: "minecraft:polished_granite_stairs", coarse: "minecraft:granite_stairs" },
+  { polished: "minecraft:polished_tuff", coarse: "minecraft:tuff" },
+  { polished: "minecraft:polished_tuff_double_slab", coarse: "minecraft:tuff_double_slab" },
+  { polished: "minecraft:polished_tuff_slab", coarse: "minecraft:tuff_slab" },
+  { polished: "minecraft:polished_tuff_stairs", coarse: "minecraft:tuff_stairs" },
+  { polished: "minecraft:polished_tuff_wall", coarse: "minecraft:tuff_wall" }
+];
+var chiseledStone = [
+  { chiseled: "minecraft:chiseled_tuff", carved: "minecraft:tuff" },
+  { chiseled: "minecraft:chiseled_tuff_bricks", carved: "minecraft:tuff_bricks" },
+  { chiseled: "minecraft:chiseled_polished_blackstone", carved: "minecraft:polished_blackstone" },
+  { chiseled: "minecraft:chiseled_nether_bricks", carved: "minecraft:nether_brick" },
+  { chiseled: "minecraft:chiseled_purpur_block", carved: "minecraft:purpur_block" },
+  { chiseled: "minecraft:infested_chiseled_stone_bricks", carved: "minecraft:infested_stone_bricks" },
+  { chiseled: "minecraft:chiseled_quartz_block", carved: "minecraft:quartz_block" },
+  { chiseled: "minecraft:chiseled_red_sandstone", carved: "minecraft:red_sandstone" },
+  { chiseled: "minecraft:chiseled_sandstone", carved: "minecraft:sandstone" },
+  { chiseled: "minecraft:chiseled_stone_bricks", carved: "minecraft:stone_bricks" },
+  { chiseled: "minecraft:chiseled_resin_bricks", carved: "minecraft:resin_bricks" },
+  { chiseled: "minecraft:chiseled_deepslate", carved: "minecraft:deepslate" },
+  { chiseled: "minecraft:chiseled_copper", carved: "minecraft:copper" },
+  { chiseled: "minecraft:exposed_chiseled_copper", carved: "minecraft:exposed_copper" },
+  { chiseled: "minecraft:oxidized_chiseled_copper", carved: "minecraft:oxidized_copper" },
+  { chiseled: "minecraft:waxed_chiseled_copper", carved: "minecraft:waxed_copper" },
+  { chiseled: "minecraft:waxed_exposed_chiseled_copper", carved: "minecraft:waxed_exposed_copper" },
+  { chiseled: "minecraft:waxed_oxidized_chiseled_copper", carved: "minecraft:waxed_oxidized_copper" },
+  { chiseled: "minecraft:waxed_weathered_chiseled_copper", carved: "minecraft:waxed_weathered_copper" },
+  { chiseled: "minecraft:weathered_chiseled_copper", carved: "minecraft:weathered_copper" }
 ];
 async function polishBlock(event) {
   try {
@@ -11089,13 +11123,17 @@ async function polishBlock(event) {
       let value = nextValue.value;
       if (value.y >= -64) {
         let breakBlock = block.dimension.getBlock(value);
-        if (breakBlock != void 0) {
-          if (breakBlock.typeId.startsWith("minecraft:weathered_")) {
-            breakBlock.dimension.setBlockType(breakBlock.location, breakBlock.typeId.split("weathered_").join(""));
-          } else if (breakBlock.typeId.startsWith("minecraft:oxidized_")) {
-            breakBlock.dimension.setBlockType(breakBlock.location, breakBlock.typeId.split("oxidized_").join(""));
-          } else if (polishedStone.indexOf(breakBlock.typeId) != -1) {
-            breakBlock.dimension.setBlockType(breakBlock.location, "polished_" + polishedStone.filter((stone) => stone == breakBlock.typeId)[0]);
+        if (breakBlock != void 0 && breakBlock.typeId != MinecraftBlockTypes.Air) {
+          if (polishedStone.some((obj) => obj.coarse == breakBlock.typeId)) {
+            let polisBlock = polishedStone.find((obj) => obj.coarse == breakBlock.typeId);
+            if (polisBlock != void 0) {
+              breakBlock.dimension.setBlockType(breakBlock.location, polisBlock.polished);
+            }
+          } else if (chiseledStone.some((obj) => obj.carved == breakBlock.typeId)) {
+            let chiseledBlock = chiseledStone.find((obj) => obj.carved == breakBlock.typeId);
+            if (chiseledBlock != void 0) {
+              breakBlock.dimension.setBlockType(breakBlock.location, chiseledBlock.chiseled);
+            }
           }
         }
       }
